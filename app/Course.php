99ddcae2bc582634ad22c8df8bfc8cc397464a65
Samuel Rojas <samuel.rojasp@gmail.com>
2020-04-16 21:53:29 -0400

    crea modelo Course con reglas de validacion y relaciones

---
+++ app/Course.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    protected $fillable = [
+        'name',
+        'code'
+     ];
+
+    protected $dates = [];
+
+    public static $putRules = [
+        'code' => 'unique:courses|max:4'
+    ];
+
+    public static $postRules = [
+        'name' => 'required',
+        'code' => 'required|unique:courses|max:4'
+    ];
+
+    public static $customMessages = [
+        // ...
+    ];
+
+    public function students()
+    {
+        return $this->hasMany('App\Student');
+    }
+}
